cb06702703b10fd8457387dbec0179481d20f79e
Gabriel Maiolli <maiolligabriel@gmail.com>
2024-06-28 14:33:05 -0300

    Update cota.c

--- trabalhos facul/cota.c
+++ trabalhos facul/cota.c
@@ -2,25 +2,27 @@
 
 int main(void) {
     
-    float tax1, tax2, tax3, taxafinal, entrada, valorfinal;
-
-    tax1 = 1.2;
-    tax2 = 1.17;
-    tax3 = 1.6;
+    float entradareais, entradadolar, valorfinal, resulie, resulimf, cotacao;
 
     printf("Digite o valor do produto em reais: \n");
-    scanf("%f", &entrada);
+    scanf("%f", &entradareais);
+    printf("Digite o valor em do produto em dolar: \n");
+    scanf("%f", &entradadolar);
 
-    if (entrada < 268) {
-        valorfinal = entrada * tax1 * tax2;
+    cotacao = entradareais / entradadolar;
+    
+    resulimf = (entradadolar * 1.2) - entradadolar;
+
+    if(entradadolar < 50) {
+        valorfinal = entradadolar * 1.2 * 1.17;
+        resulie = (entradadolar * 1.17) - entradadolar;
     }
-    else if (entrada >= 268) {
-        valorfinal = entrada * tax1 * tax3;
+    else if ( entradadolar >= 50) {
+        valorfinal = entradadolar * 1.2 * 1.6;
+        resulie = (entradadolar * 1.6) - entradadolar;
     }
 
-    taxafinal = valorfinal - entrada;
-
-    printf("Seu produto passou a custar R$%.2f pois teve a cobranca de taxa equivalente a: R$%.2f", valorfinal, taxafinal);
+    printf("Seu produto passou a custar R$%.2f pois teve a cobranca de imposto federal equivalente a: R$%.2f e mais R$%.2f de imposto estadual", valorfinal * cotacao, resulie * cotacao, resulimf * cotacao);
 
     return 0;
 }
