b69b6662e31cf9b657cc8eb4853fbd90e2e62de4
Gabriel Maiolli <maiolligabriel@gmail.com>
2024-06-28 14:41:41 -0300

    .

--- trabalhos facul/testep.c
+++ trabalhos facul/testep.c
@@ -4,9 +4,12 @@ int main() {
     char tipo;
     printf("Pessoa fisica ou juridica (F/J): ");
     scanf("%c", &tipo);
-    if (tipo == 'F')
+    if (tipo == 'F' || tipo == 'f')
         printf("Voce possui um RG e um CPF");
-    else printf("Voce possui um CNPJ e uma Inscricao Estadual");
+    else if(tipo == 'J' || tipo == 'j') printf("Voce possui um CNPJ e uma Inscricao Estadual");
+    else {
+        printf("Digite apenas F ou J! \n");
+    }
 
 return 0;
 }
