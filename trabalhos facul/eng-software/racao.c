9378de7bd39e27658c9b0de8372d7aa863a3e90f
Gabriel Maiolli <maiolligabriel@gmail.com>
2025-03-26 09:02:33 -0300

    .

--- trabalhos facul/eng-software/racao.c
+++ trabalhos facul/eng-software/racao.c
@@ -4,12 +4,12 @@ float pesosaco,racgato1,racgato2,racdia5;
 
 main(){
     printf("Qual o peso do saco de racao em quilos:");
-    scanf("%f",pesosaco);
-    printf("Qual a quantidade, em gramas, para o primeiro gato;");
-    scanf("%f",racgato1);
-    printf("Qual a quantidade, em gramas, para o segundo gato;");
-    scanf("%f",racgato2);
+    scanf("%f",&pesosaco);
+    printf("Qual a quantidade, em gramas, para o primeiro gato:");
+    scanf("%f",&racgato1);
+    printf("Qual a quantidade, em gramas, para o segundo gato:");
+    scanf("%f",&racgato2);
 
     racdia5 = pesosaco - (((racgato1/1000) + (racgato2/1000)) * 5);
-    printf("Sobrara %f kg de racao",racdia5);
+    printf("Sobrara %.3f kg de racao",racdia5);
 }
